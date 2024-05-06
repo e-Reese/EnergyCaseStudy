42c0b9c35a83fe6cef91997e347a195b178dfef3
Ethan-Orion-Reese <ethanorionreese@gmail.com>
2024-05-06 14:22:37 -0700

    Commenting code in SQL scripts

--- code-sql/Tableau_CleanElecGen.sql
+++ code-sql/Tableau_CleanElecGen.sql
@@ -1,17 +1,21 @@
-SELECT DISTINCT SUBSTRING(period, 1,4) AS year, location, stateDescription, fueltypeid, fuelTypeDescription, 
-	SUM([total-consumption]) OVER (PARTITION BY location, fueltypeid) AS [total-annual-consumption], 
-	[total-consumption-units], 
-	SUM(generation) OVER (PARTITION BY location, fueltypeid) AS [total-annual-generation], 
+SELECT DISTINCT SUBSTRING(period, 1,4) AS year, -- Get the year only. Month not needed because we are summing for the entire year
+	location,
+	stateDescription,
+	fueltypeid,
+	fuelTypeDescription,
+	SUM([total-consumption]) OVER (PARTITION BY location, fueltypeid) AS [total-annual-consumption], -- Sum each month by state and fuel type. Outputs total electricity CONSUMPTION per sate per fuel type.
+	[total-consumption-units],
+	SUM(generation) OVER (PARTITION BY location, fueltypeid) AS [total-annual-generation], -- Sum each month by state and fuel type. Outputs total electricity GENERATED per sate per fuel type.
 	[generation-units]
 INTO ElecGen_Annual_2023
 FROM ElectricityGen_Monthly_2023
-WHERE location IN ('AK', 'AL', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 
+WHERE location IN ('AK', 'AL', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN',
 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
-'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY') AND fueltypeid IN ('ALL', 'COW', 'REN', 'NGO', 'NUC', 'PET')
+'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY') AND fueltypeid IN ('ALL', 'COW', 'REN', 'NGO', 'NUC', 'PET') -- Select only the 50 states and major fuel types.
 ORDER BY location, [total-annual-generation] DESC
 
-SELECT * FROM ElecGen_Annual_2023 AS a WHERE 
-(SELECT COUNT(*) FROM ElecGen_Annual_2023 AS b 
-WHERE b.stateDescription = a.stateDescription AND b.[total-annual-generation] >= a.[total-annual-generation]) <= 4 
+SELECT * FROM ElecGen_Annual_2023 AS a WHERE -- Filtering the data down to just the top 4 fuel sources (including the categroy of "all fuels") to make things easier in tableau
+(SELECT COUNT(*) FROM ElecGen_Annual_2023 AS b
+WHERE b.stateDescription = a.stateDescription AND b.[total-annual-generation] >= a.[total-annual-generation]) <= 4
 ORDER BY a.stateDescription DESC, a.[total-annual-generation] DESC
 	
--- code-sql/Tableua_CleanElecSales.sql
+++ code-sql/Tableua_CleanElecSales.sql
@@ -1,5 +1,5 @@
-SELECT period, stateid, stateDescription, sectorid, sectorName, customers, price, revenue, sales, [customers-units], [price-units], [revenue-units], [sales-units]
+SELECT period, stateid, stateDescription, sectorid, sectorName, customers, price, revenue, sales, [customers-units], [price-units], [revenue-units], [sales-units] -- Select all data, why not.
 FROM ElectricitySales_Monthly_2023
-WHERE sectorid != 'OTH' AND stateid IN ('AK', 'AL', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 
+WHERE sectorid != 'OTH' AND stateid IN ('AK', 'AL', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN',
 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
-'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY')
+'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY') -- Only get the states we want because the data was exported with regions and D.C. as well.
